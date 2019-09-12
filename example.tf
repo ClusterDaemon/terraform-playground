ceb794332e9a88c68a8bcfb38109c29c9a5bcb28
dhay <dhay@linbit.com>
2019-09-12 07:34:18 -0700

    yeet

--- example.tf
+++ example.tf
@@ -1,17 +1,16 @@
 provider "aws" {
   profile    = "default"
   region     = "us-west-2"
-  tags = {
-    Name = "example"
-  }
 }
 
 resource "aws_vpc" "example_vpc" {
   cidr_block = "10.100.0.0/16"
   enable_dns_hostnames = true
+
   tags = {
     Name = "example"
   }
+
 }
 
 resource "aws_internet_gateway" "example_gw" {
@@ -20,39 +19,72 @@ resource "aws_internet_gateway" "example_gw" {
 
 resource "aws_subnet" "example_subnet_a" {
   vpc_id = "${aws_vpc.example_vpc.id}"
-  cidr_block = "10.100.0.0/24"
+  cidr_block = "10.100.1.0/24"
   availability_zone = "us-west-2a"
+
   tags = {
     Name = "example"
   }
-  depends_on = ["aws_internet_gateway.example_gw"]
+
+}
+
+resource "aws_subnet" "example_subnet_b" {
+  vpc_id = "${aws_vpc.example_vpc.id}"
+  cidr_block = "10.100.2.0/24"
+  availability_zone = "us-west-2b"
+
+  tags = {
+    Name = "example_1"
+  }
+
 }
 
 resource "aws_instance" "example_instance" {
-  ami           = "ami-b374d5a5"
+  ami           = "ami-06f2f779464715dc5"
   instance_type = "t2.micro"
-  network_interface {
-    network_interface_id = "${aws_network_interface.example_interface.id}"
-    device_index = 0
-  }
-  tags = {
-    Name = "example"
-  }
-}
-
-resource "aws_network_interface" "example_interface" {
   subnet_id = "${aws_subnet.example_subnet_a.id}"
-  private_ips = ["10.100.0.1"]
+
   tags = {
     Name = "example"
   }
+
 }
 
-resource "aws_eip" "ip" {
+resource "aws_instance" "example_1_instance" {
+  ami = "ami-06f2f779464715dc5"
+  instance_type = "t2.micro"
+  subnet_id = "${aws_subnet.example_subnet_b.id}"
+
+  tags = {
+    Name = "example_1"
+  }
+
+}
+
+resource "aws_eip" "example_eip" {
   vpc = true
   instance = "${aws_instance.example_instance.id}"
+
   tags = {
     Name = "example"
   }
-  depends_on = ["aws_internet_gateway.example_gw"]
+
+  provisioner "local-exec" {
+    command = "echo ${aws_eip.example_eip.public_ip} > example_instance.public_ip.txt"
+  }
+
+}
+
+resource "aws_eip" "example_1_eip" {
+  vpc = true
+  instance = "${aws_instance.example_1_instance.id}"
+
+  tags = {
+    Name = "example_1"
+  }
+
+  provisioner "local-exec" {
+    command = "echo ${aws_eip.example_1_eip.public_ip} > example_1_instance.public_ip.txt"
+  }
+
 }
