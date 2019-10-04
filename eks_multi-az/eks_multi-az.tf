25f71725e184e2543eb22409cb872948b402a52e
dhay <dhay@linbit.com>
2019-10-03 21:06:45 -0700

    cleaned up the manifest quite a bit, converted most things to variables, made subnets resources calulate their own CIDRs, got rid of nonfunctional (in eks, for now) IAM policy for openid connect.

--- eks_multi-az/eks_multi-az.tf
+++ eks_multi-az/eks_multi-az.tf
@@ -1,20 +1,23 @@
-# Terraform config for the SDC2019 conference. This will establish an EKS cluster in AWS with an auto-scaling worker pool, providing the wielder with a valid kubectl config.
-#
-# I'll freshen this up later and make it more vairable / dynamic. For the purposes of this demo, it's more than good enough.
-#
-# A lot of this is just cobblework for EKS - we neeed a functioning VPC for it to chill in and a few policies defined.
+# Establishes an EKS cluster in AWS with an auto-scaling worker pool, providing the wielder with a valid kubectl config.
+# Assumes that aws_cli is installed and configured with valid credentials
+# Assumes that aws_iam_authenticator binary can be found via $PATH
 
 variable "region" {
   default = "us-west-2"
   type = string
 }
 
-variable "cluster-name" {
-  default = "sdc2019-demo"
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+  type = string
+}
+
+variable "cluster_name" {
+  default = "linbit-eks"
   type    = string
 }
 
-variable "kubectl-cidr" {
+variable "kubectl_cidr" {
   default = ["0.0.0.0/32"]
   type = list
 }
@@ -24,18 +27,33 @@ variable "worker_instance_type" {
   type = string
 }
 
-variable "linstor_sp_size" {
+variable "worker_disk_size" {
   default = 100
   type = number
 }
 
 variable "pem_key" {
-  default = "sdc2019-demo"
+  default = "linbit_eks"
   type = string
 }
 
 variable "worker_node_count" {
   default = 3
+  type = number
+}
+
+variable "worker_node_max_count" {
+  default = 3
+  type = number
+}
+
+variable "worker_node_min_count" {
+  default = 1
+  type = number
+}
+
+variable "kube_version" {
+  default = 1.14
   type = number
 }
 
@@ -46,60 +64,60 @@ provider "aws" {
 
 data "aws_availability_zones" "available" {}
 
-resource "aws_vpc" "demo" {
-  cidr_block = "10.1.0.0/16"
+resource "aws_vpc" "eks" {
+  cidr_block = var.vpc_cidr
 
   tags = "${
     map(
-     "Name", "sdc2019-demo-node",
-     "kubernetes.io/cluster/${var.cluster-name}", "shared",
+     "Name", "${var.cluster_name}",
+     "kubernetes.io/cluster/${var.cluster_name}", "shared",
     )
   }"
 }
 
-resource "aws_subnet" "demo" {
+resource "aws_subnet" "eks" {
   count = 3
 
   availability_zone = "${data.aws_availability_zones.available.names[count.index]}"
-  cidr_block        = "10.1.${count.index}.0/24"
-  vpc_id            = "${aws_vpc.demo.id}"
+  cidr_block        = "${cidrsubnet((var.vpc_cidr), 8, count.index)}"
+  vpc_id            = "${aws_vpc.eks.id}"
 
   tags = "${
     map(
-     "Name", "sdc2019-demo",
-     "kubernetes.io/cluster/${var.cluster-name}", "shared",
+     "Name", "${var.cluster_name}",
+     "kubernetes.io/cluster/${var.cluster_name}", "shared",
     )
   }"
 }
 
-resource "aws_internet_gateway" "demo" {
-  vpc_id = "${aws_vpc.demo.id}"
+resource "aws_internet_gateway" "eks" {
+  vpc_id = "${aws_vpc.eks.id}"
 
   tags = {
-    Name = "sdc2019-demo"
+    Name = var.cluster_name
   }
 }
 
-resource "aws_route_table" "demo" {
-  vpc_id = "${aws_vpc.demo.id}"
+resource "aws_route_table" "eks" {
+  vpc_id = "${aws_vpc.eks.id}"
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_internet_gateway.demo.id}"
+    gateway_id = "${aws_internet_gateway.eks.id}"
   }
 }
 
-resource "aws_route_table_association" "demo" {
+resource "aws_route_table_association" "eks" {
   count = 3
 
-  subnet_id      = "${aws_subnet.demo.*.id[count.index]}"
-  route_table_id = "${aws_route_table.demo.id}"
+  subnet_id      = "${aws_subnet.eks.*.id[count.index]}"
+  route_table_id = "${aws_route_table.eks.id}"
 }
 
 # We need IAM policy configuration to allow EKS to grab data from other AWS services.
 
-resource "aws_iam_role" "demo-cluster" {
-  name = "sdc2019-demo-cluster"
+resource "aws_iam_role" "eks" {
+  name = var.cluster_name
 
   assume_role_policy = <<POLICY
 {
@@ -117,22 +135,20 @@ resource "aws_iam_role" "demo-cluster" {
 POLICY
 }
 
-resource "aws_iam_role_policy_attachment" "demo-cluster-AmazonEKSClusterPolicy" {
+resource "aws_iam_role_policy_attachment" "eks-AmazonEKSClusterPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = "${aws_iam_role.demo-cluster.name}"
+  role       = "${aws_iam_role.eks.name}"
 }
 
-resource "aws_iam_role_policy_attachment" "demo-cluster-AmazonEKSServicePolicy" {
+resource "aws_iam_role_policy_attachment" "eks-AmazonEKSServicePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
-  role       = "${aws_iam_role.demo-cluster.name}"
+  role       = "${aws_iam_role.eks.name}"
 }
 
-# Security groups to control access to K8S masters
-
-resource "aws_security_group" "demo-cluster" {
-  name        = "SDC2019-demo-cluster"
-  description = "Cluster communication with worker nodes"
-  vpc_id      = "${aws_vpc.demo.id}"
+resource "aws_security_group" "eks" {
+  name        = var.cluster_name
+  description = "Control access to K8S masters"
+  vpc_id      = "${aws_vpc.eks.id}"
 
   egress {
     from_port   = 0
@@ -142,18 +158,18 @@ resource "aws_security_group" "demo-cluster" {
   }
 
   tags = {
-    Name = "SDC2019-demo"
+    Name = var.cluster_name
   }
 }
 
 # Define what addresses may access kubectl
 
-resource "aws_security_group_rule" "demo-cluster-ingress-workstation-https" {
-  cidr_blocks       = var.kubectl-cidr
+resource "aws_security_group_rule" "eks-ingress-workstation-https" {
+  cidr_blocks       = var.kubectl_cidr
   description       = "Allow workstation to communicate with the cluster API Server"
   from_port         = 443
   protocol          = "tcp"
-  security_group_id = "${aws_security_group.demo-cluster.id}"
+  security_group_id = "${aws_security_group.eks.id}"
   to_port           = 443
   type              = "ingress"
 }
@@ -162,26 +178,27 @@ resource "aws_security_group_rule" "demo-cluster-ingress-workstation-https" {
 # I would rather use an expression to reference any number of subnet_ids
 # but I have no idea why a splat expression isn't working
 # so I hard-coded the list of indexed resources like a chump.
+# "${aws_subnet.eks[0].id}","${aws_subnet.eks[1].id}","${aws_subnet.eks[2].id}" 
 
-resource "aws_eks_cluster" "demo" {
-  name            = "${var.cluster-name}"
-  role_arn        = "${aws_iam_role.demo-cluster.arn}"
-  version         = "1.14"
+resource "aws_eks_cluster" "eks" {
+  name            = var.cluster_name
+  role_arn        = "${aws_iam_role.eks.arn}"
+  version         = var.kube_version
 
   vpc_config {
-    security_group_ids = ["${aws_security_group.demo-cluster.id}"]
-    subnet_ids         = ["${aws_subnet.demo[0].id}","${aws_subnet.demo[1].id}","${aws_subnet.demo[2].id}"]
+    security_group_ids = ["${aws_security_group.eks.id}"]
+    subnet_ids         = ["${aws_subnet.eks[0].id}","${aws_subnet.eks[1].id}","${aws_subnet.eks[2].id}"]
   }
 
   depends_on = [
-    "aws_iam_role_policy_attachment.demo-cluster-AmazonEKSClusterPolicy",
-    "aws_iam_role_policy_attachment.demo-cluster-AmazonEKSServicePolicy",
+    "aws_iam_role_policy_attachment.eks-AmazonEKSClusterPolicy",
+    "aws_iam_role_policy_attachment.eks-AmazonEKSServicePolicy",
   ]
 }
 
 # You'll want to get to kubectl at this point.
 # The AWS CLI "eks update-kubeconfig" command provides a simple method to create or update configuration files.
-# However, the configuration below should spit a valid kubectl config at you on apply or query to use as you will.
+# However, the configuration below should spit a valid kubectl config at you on apply or output to use as you will.
 
 locals {
   kubeconfig = <<KUBECONFIG
@@ -190,8 +207,8 @@ locals {
 apiVersion: v1
 clusters:
 - cluster:
-    server: ${aws_eks_cluster.demo.endpoint}
-    certificate-authority-data: ${aws_eks_cluster.demo.certificate_authority.0.data}
+    server: ${aws_eks_cluster.eks.endpoint}
+    certificate-authority-data: ${aws_eks_cluster.eks.certificate_authority.0.data}
   name: kubernetes
 contexts:
 - context:
@@ -210,7 +227,7 @@ users:
       args:
         - "token"
         - "-i"
-        - "${var.cluster-name}"
+        - "${var.cluster_name}"
 KUBECONFIG
 }
 
@@ -220,8 +237,8 @@ output "kubeconfig" {
 
 # Worker node IAM policies
 
-resource "aws_iam_role" "demo-node" {
-  name = "sdc2019-demo-node"
+resource "aws_iam_role" "eks_worker" {
+  name = "eks_worker"
 
   assume_role_policy = <<POLICY
 {
@@ -239,63 +256,32 @@ resource "aws_iam_role" "demo-node" {
 POLICY
 }
 
-resource "aws_iam_role_policy_attachment" "demo-node-AmazonEKSWorkerNodePolicy" {
+resource "aws_iam_role_policy_attachment" "eks_worker-AmazonEKSWorkerNodePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = "${aws_iam_role.demo-node.name}"
+  role       = "${aws_iam_role.eks_worker.name}"
 }
 
-resource "aws_iam_role_policy_attachment" "demo-node-AmazonEKS_CNI_Policy" {
+resource "aws_iam_role_policy_attachment" "eks_worker-AmazonEKS_CNI_Policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = "${aws_iam_role.demo-node.name}"
+  role       = "${aws_iam_role.eks_worker.name}"
 }
 
-resource "aws_iam_role_policy_attachment" "demo-node-AmazonEC2ContainerRegistryReadOnly" {
+resource "aws_iam_role_policy_attachment" "eks_worker-AmazonEC2ContainerRegistryReadOnly" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = "${aws_iam_role.demo-node.name}"
+  role       = "${aws_iam_role.eks_worker.name}"
 }
 
-resource "aws_iam_instance_profile" "demo-node" {
-  name = "sdc2019-demo"
-  role = "${aws_iam_role.demo-node.name}"
-}
-
-resource "aws_iam_openid_connect_provider" "demo" {
-  client_id_list        = ["sts.amazonaws.com"]
-  thumbprint_list       = []
-  url                   = "${aws_eks_cluster.demo.identity.0.oidc.0.issuer}"
-}
-
-data "aws_caller_identity" "current" {}
-
-data "aws_iam_policy_document" "demo_assume_role_policy" {
-  statement {
-    actions = ["sts:AssumeRoleWithWebIdentity"]
-    effect  = "Allow"
-
-    condition {
-      test = "StringEquals"
-      variable = "${replace(aws_iam_openid_connect_provider.demo.url, "https://", "")}:sub"
-      values = ["system:serviceaccount:kube-system:aws-node"]
-    }
-
-    principals {
-      identifiers =["${aws_iam_openid_connect_provider.demo.arn}"]
-      type = "Federated"
-    }
-  }
-}
-
-resource "aws_iam_role" "demo-node-sa-role" {
-  assume_role_policy = "${data.aws_iam_policy_document.demo_assume_role_policy.json}"
-  name = "demo-node-sa-role"
+resource "aws_iam_instance_profile" "eks_worker" {
+  name = "eks_worker"
+  role = "${aws_iam_role.eks_worker.name}"
 }
 
 # Worker node security groups
 
-resource "aws_security_group" "demo-node" {
-  name        = "sdc2019-demo-node"
+resource "aws_security_group" "eks_nodes" {
+  name        = "eks_nodes"
   description = "Security group for all nodes in the cluster"
-  vpc_id      = "${aws_vpc.demo.id}"
+  vpc_id      = "${aws_vpc.eks.id}"
 
   egress {
     from_port   = 0
@@ -306,85 +292,81 @@ resource "aws_security_group" "demo-node" {
 
   tags = "${
     map(
-     "Name", "sdc2019-demo-node",
-     "kubernetes.io/cluster/${var.cluster-name}", "owned",
+     "Name", "eks_nodes",
+     "kubernetes.io/cluster/${var.cluster_name}", "owned",
     )
   }"
 }
 
-resource "aws_security_group_rule" "demo-node-ingress-self" {
+resource "aws_security_group_rule" "eks_nodes-ingress_self" {
   description              = "Allow nodes to communicate with each other"
   from_port                = 0
   protocol                 = "-1"
-  security_group_id        = "${aws_security_group.demo-node.id}"
-  source_security_group_id = "${aws_security_group.demo-node.id}"
+  security_group_id        = "${aws_security_group.eks_nodes.id}"
+  source_security_group_id = "${aws_security_group.eks_nodes.id}"
   to_port                  = 65535
   type                     = "ingress"
 }
 
-resource "aws_security_group_rule" "demo-node-ingress-cluster" {
+resource "aws_security_group_rule" "eks_nodes-ingress_cluster" {
   description              = "Allow worker Kubelets and pods to receive communication from the cluster control plane"
   from_port                = 1025
   protocol                 = "tcp"
-  security_group_id        = "${aws_security_group.demo-node.id}"
-  source_security_group_id = "${aws_security_group.demo-cluster.id}"
+  security_group_id        = "${aws_security_group.eks_nodes.id}"
+  source_security_group_id = "${aws_security_group.eks.id}"
   to_port                  = 65535
   type                     = "ingress"
 }
 
-# Worker node access to EKS master cluster
-
-resource "aws_security_group_rule" "demo-cluster-ingress-node-https" {
+resource "aws_security_group_rule" "eks-ingress-eks_nodes-https" {
   description              = "Allow pods to communicate with the cluster API Server"
   from_port                = 443
   protocol                 = "tcp"
-  security_group_id        = "${aws_security_group.demo-cluster.id}"
-  source_security_group_id = "${aws_security_group.demo-node.id}"
+  security_group_id        = "${aws_security_group.eks.id}"
+  source_security_group_id = "${aws_security_group.eks_nodes.id}"
   to_port                  = 443
   type                     = "ingress"
 }
 
-# Worker node auto-scaling EKS-friendly AMI definition
+# Worker node EKS-friendly AMI definition (Amazon Linux)
 
-data "aws_ami" "eks-worker" {
+data "aws_ami" "eks_worker" {
   filter {
     name   = "name"
-    values = ["amazon-eks-node-${aws_eks_cluster.demo.version}-v*"]
+    values = ["amazon-eks-node-${aws_eks_cluster.eks.version}-v*"]
   }
 
   most_recent = true
   owners      = ["602401143452"] # Amazon EKS AMI Account ID
 }
 
-# Worker node auto-scaling launch configuration
-
 data "aws_region" "current" {}
 
 # Insert necessary bootstrap commands into worker node userdata
 locals {
-  demo-node-userdata = <<USERDATA
+  eks-worker-userdata = <<USERDATA
 #!/bin/bash
 set -o xtrace
-/etc/eks/bootstrap.sh --apiserver-endpoint '${aws_eks_cluster.demo.endpoint}' --b64-cluster-ca '${aws_eks_cluster.demo.certificate_authority.0.data}' '${var.cluster-name}'
+/etc/eks/bootstrap.sh --apiserver-endpoint '${aws_eks_cluster.eks.endpoint}' --b64-cluster-ca '${aws_eks_cluster.eks.certificate_authority.0.data}' '${var.cluster_name}'
 vgcreate vg0 /dev/sdb
 lvcreate -T -l 100%FREE -n thin-lvm vg0
 yum -y install kernel-headers-$(uname -r)
 USERDATA
 }
 
-resource "aws_launch_configuration" "demo" {
+resource "aws_launch_configuration" "eks_worker" {
   associate_public_ip_address = true
-  iam_instance_profile        = "${aws_iam_instance_profile.demo-node.name}"
-  image_id                    = "${data.aws_ami.eks-worker.id}"
+  iam_instance_profile        = "${aws_iam_instance_profile.eks_worker.name}"
+  image_id                    = "${data.aws_ami.eks_worker.id}"
   instance_type               = var.worker_instance_type
   key_name                    = var.pem_key
-  name_prefix                 = "sdc2019-demo"
-  security_groups             = ["${aws_security_group.demo-node.id}"]
-  user_data_base64            = "${base64encode(local.demo-node-userdata)}"
+  name_prefix                 = "eks"
+  security_groups             = ["${aws_security_group.eks_nodes.id}"]
+  user_data_base64            = "${base64encode(local.eks-worker-userdata)}"
 
   ebs_block_device {
     device_name = "/dev/sdb"
-    volume_size = var.linstor_sp_size
+    volume_size = var.worker_disk_size
   }
 
   lifecycle {
@@ -394,22 +376,22 @@ resource "aws_launch_configuration" "demo" {
 
 # Worker node auto-scaling group itself
 
-resource "aws_autoscaling_group" "demo" {
+resource "aws_autoscaling_group" "eks_nodes" {
   desired_capacity     = var.worker_node_count
-  launch_configuration = "${aws_launch_configuration.demo.id}"
-  max_size             = var.worker_node_count
-  min_size             = 1
-  name                 = "sdc2019-demo"
-  vpc_zone_identifier  = ["${aws_subnet.demo[0].id}","${aws_subnet.demo[1].id}","${aws_subnet.demo[2].id}",]
+  launch_configuration = "${aws_launch_configuration.eks_worker.id}"
+  max_size             = var.worker_node_max_count
+  min_size             = var.worker_node_min_count
+  name                 = var.cluster_name
+  vpc_zone_identifier  = ["${aws_subnet.eks[0].id}","${aws_subnet.eks[1].id}","${aws_subnet.eks[2].id}",]
 
   tag {
     key                 = "Name"
-    value               = "sdc2019-demo"
+    value               = var.cluster_name
     propagate_at_launch = true
   }
 
   tag {
-    key                 = "kubernetes.io/cluster/${var.cluster-name}"
+    key                 = "kubernetes.io/cluster/${var.cluster_name}"
     value               = "owned"
     propagate_at_launch = true
   }
@@ -431,7 +413,7 @@ metadata:
   namespace: kube-system
 data:
   mapRoles: |
-    - rolearn: ${aws_iam_role.demo-node.arn}
+    - rolearn: ${aws_iam_role.eks_worker.arn}
       username: system:node:{{EC2PrivateDNSName}}
       groups:
         - system:bootstrappers
